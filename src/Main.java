432b22277b5cb49ba764693805d606ce821255fb
Dahakarful <Ragonda@DESKTOP-Ragonda>
2016-01-12 17:21:43 +0100

    main, parametre

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) throws IOException {
+		String implementation = ParametreFichier.getParametre("Implementation");
+		if(implementation.equals("bas_niveau")){
+			
+		}else if(implementation.equals("haut_niveau")){
+			Serveur.main(null);
+			Client.main(null);
+		}else{
+			System.out.println("L'implementation choisi est non valide !");
+		}
 	}
 
 }
