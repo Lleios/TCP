5e032053ef65d4bb1fb6df03e2aadfe31284b012
Dahakarful <Dahakarful @DESKTOP-Ragonda>
2016-01-12 15:34:11 +0100

    TCP Client/Serveur

--- src/Client.java
+++ src/Client.java
@@ -1,14 +1,13 @@
 import java.io.IOException;
-import java.net.*;
+import java.net.Socket;
 
-public class Client {
+public class Client{
 
 	public static void main(String[] zero){
-		
 		Socket socket;
 		try {
-		socket = new Socket("localhost",2009);
-		socket.close();
+			socket = new Socket("localhost",2009);
+			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/Serveur.java
+++ src/Serveur.java
@@ -1,33 +1,17 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Properties;
 
 public class Serveur {
 
 	public static void main(String[] zero) throws IOException{
 		
-		Properties properties = new Properties();
-		FileInputStream fileStream = null;
-		try {
-			fileStream = new FileInputStream("../../config.txt");
-			properties.load(fileStream);
-			System.out.println(properties.getProperty("Port"));
-			System.out.println(properties.getProperty("IP"));
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}finally {
-		      fileStream.close();
-		}
-		
 		ServerSocket socket;
 		try {
-		socket = new ServerSocket(2009);
-		Thread t = new Thread(new Accepter_clients(socket));
-		t.start();
-		System.out.println("Mes employeurs sont prêts !");
+			socket = new ServerSocket(2009);
+			Thread t = new Thread(new Accepter_clients(socket));
+			t.start();
+			System.out.println("Mes employeurs sont prêts !");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -45,15 +29,13 @@ class Accepter_clients implements Runnable {
 	   }
 		
 		public void run() {
-
 	        try {
 	        	while(true){
-			  socket = socketserver.accept(); // Un client se connecte on l'accepte
-	                  System.out.println("Le client numéro "+nbrclient+ " est connecté !");
-	                  nbrclient++;
-	                  socket.close();
+	        		socket = socketserver.accept(); // Un client se connecte on l'accepte
+	                System.out.println("Le client numéro " + nbrclient + " est connecté !");
+	                nbrclient++;
+	                socket.close();
 	        	}
-	        
 	        } catch (IOException e) {
 				e.printStackTrace();
 			}
