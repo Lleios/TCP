2618a99568275ba36a3b623112007848f2f2967f
Lleios <kevin.gonnord@outlook.com>
2016-01-12 19:18:11 +0100

    conflit

--- src/ParametreFichier.java
+++ src/ParametreFichier.java
@@ -1,39 +1,10 @@
-<<<<<<< HEAD
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-
-public class ParametreFichier {
-	
-	public ParametreFichier (){
-		
-	}
-	
-	static public String getParametre(String prop) throws IOException{
-		Properties properties = new Properties();
-		FileInputStream fileStream = null;
-		String res = "";
-		try {
-			fileStream = new FileInputStream("config.txt");
-			properties.load(fileStream);
-			res = properties.getProperty(prop);
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}finally {
-		      fileStream.close();
-		}
-		return res;
-	}
-}
-=======
 /**
 * Classe ParametreFichier
 *
 * la classe ParametreFichier permet de configurer : - le port utilisé
 * 													- durée d'inactivité du client avant sa déconnexion
 * 													- le nombre maximum de connexion simultanée
-* 													- le type d'implémentation 
+* 													- le type d'implémentation
 *
 * @author  Gonnord Kevin, Chcouropat Youri
 */
@@ -44,11 +15,11 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ParametreFichier {
-	
+
 	public ParametreFichier (){
-		
+
 	}
-	
+
 	static public String getParametre(String prop) throws IOException{
 		Properties properties = new Properties();
 		FileInputStream fileStream = null;
@@ -65,4 +36,3 @@ public class ParametreFichier {
 		return res;
 	}
 }
->>>>>>> origin/master
