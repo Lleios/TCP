40d5d7b21d15c06d4e0bb97bb9a9563da173adfe
Dahakarful <Ragonda@DESKTOP-Ragonda>
2016-01-15 21:21:29 +0100

    ClientEcho

--- src/autre/ClientEcho.java
+++ src/autre/ClientEcho.java
@@ -1,18 +1,17 @@
 package autre;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-  public class EchoClient {
+  public class ClientEcho {
    
-      public static final int DEFAULT_PORT = 56789;
+      public static final int PORT = 56789;
   
      public static void usage() {
-        System.out.println ("Usage: java EchoClient <hostname> [<port>]");
+        System.out.println ("Usage: java ClientEcho <hote> [<port>]");
         System.exit (0);
      }
   
@@ -21,52 +20,40 @@ import java.net.Socket;
      }
   
      public static void main (String[] args) {
-        int port = DEFAULT_PORT;
+        int port = PORT;
         Socket s = null;
-          
-        // Parse the port specification
-//        if ((args.length != 1) && (args.length != 2)) usage();
-//        if (args.length == 1) {
-           port = DEFAULT_PORT;
-//        } else {
-//           try {
-//              port = Integer.parseInt(args[1]);
-//           } catch (NumberFormatException e) {
-//              usage();
-//           }
-//        }
         
         try {
-           // Create a socket to communicate to the specified host and port
+           // Creer un socket pour communiquer avec le hote et le port specifie
            s = new Socket ("localhost", port);
-           // Create streams for reading and writing lines of text
-           // from and to this socket.
+           
+           // Creer un stream pour lire et ecrire des lignes de texte depuis et vers ce socket
            BufferedReader sin = new BufferedReader (new InputStreamReader (s.getInputStream()));
            PrintWriter sout = new PrintWriter (s.getOutputStream(), true);
-           // Create a stream for reading lines of text from the console
+           
+           // Creer un stream pour lire les lignes de texte depuis la console 
           BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
            
-           // Tell the user that we've connected
+           // Dire au client qu'il est connecte 
            msg ("connected to "+s.getInetAddress()+" at port "+s.getPort());
   
            String line;
            while (true) {
-              // print a prompt
               System.out.print ("> "); 
               System.out.flush();
-              // read a line from the console; check for EOF
+              // Lire la ligne 
               line = in.readLine();
               if (line == null) break;
               msg ("sending line to server: " + line);
               sout.println (line);
-              line = sin.readLine();       // Read a line from the server.
+              line = sin.readLine();
   
-              // Check if connection is closed (i.e. for EOF)
+              // Verifier si la connection est ferme 
               if (line == null) { 
                  msg ("connection closed by server.");
                  break;
               }
-              // And write the line to the console.
+              // Ecrire dans la console
               msg ("received from server:   " + line);
            }
         } catch (IOException e) {
